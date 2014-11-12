4e8cb090858d12a2e00c43fc760a65e4038dd6b4
kasho11 <kasho777@gmail.com>
2014-11-12 14:34:54 -0700

    Working FACESPACE!

--- func.m
+++ func.m
@@ -1,29 +1,32 @@
 flist=textread('faces.txt','%s');
 i=1;
-A=[];
+Faces=[];
 U=[];
 b = size(flist);
 while(i<b(1))
     T=imread(char(flist(i,1)));
     T=reshape(T,prod(size(T)),1);
-    A(:,size(A,2)+1)=T;
+    Faces(:,size(Faces,2)+1)=T;
 	i=i+1;
 endwhile
 
-Z=mean(A,2);
-norm = A-Z;
-C=A'*A;
-test=eigs(C,10);
-u1=Z*test(1);
-u2=Z*test(2);
-u3=Z*test(3);
-u4=Z*test(4);
-u5=Z*test(5);
-u6=Z*test(6);
-u7=Z*test(7);
-u8=Z*test(8);
-u9=Z*test(9);
-u10=Z*test(10);
+AvgFace=mean(Faces,2);
+NormFaces = Faces-AvgFace;
+AvgNormFace=mean(NormFaces,2);
+Cov=Faces'*Faces;
+[Eigens, values]=eigs(Cov);
+
+
+
+u1=NormFaces*Eigens(:,1);
+
+
+
+u2=NormFaces*Eigens(:,2);
+u3=NormFaces*Eigens(:,3);
+u4=NormFaces*Eigens(:,4);
+u5=NormFaces*Eigens(:,5);
+u6=NormFaces*Eigens(:,6);
 colormap('gray');
 i=1;
 facespace=[];
@@ -33,18 +36,20 @@ facespace(:,3)=u3;
 facespace(:,4)=u4;
 facespace(:,5)=u5;
 facespace(:,6)=u6;
-facespace(:,7)=u7;
-facespace(:,8)=u8;
-facespace(:,9)=u9;
-facespace(:,10)=u10;
+
+
+
+
 imwrite(reshape(uint8(255/(max(max(u1))-min(min(u1)))*(u1-min(min(u1)))),250,250),"pic1.jpg");
+
+
+
 imwrite(reshape(uint8(255/(max(max(u2))-min(min(u2)))*(u2-min(min(u2)))),250,250),"pic2.jpg");
+
+
+
 imwrite(reshape(uint8(255/(max(max(u3))-min(min(u3)))*(u3-min(min(u3)))),250,250),"pic3.jpg");
 imwrite(reshape(uint8(255/(max(max(u4))-min(min(u4)))*(u4-min(min(u4)))),250,250),"pic4.jpg");
 imwrite(reshape(uint8(255/(max(max(u5))-min(min(u5)))*(u5-min(min(u5)))),250,250),"pic5.jpg");
 imwrite(reshape(uint8(255/(max(max(u6))-min(min(u6)))*(u6-min(min(u6)))),250,250),"pic6.jpg");
-imwrite(reshape(uint8(255/(max(max(u7))-min(min(u7)))*(u7-min(min(u7)))),250,250),"pic7.jpg");
-imwrite(reshape(uint8(255/(max(max(u8))-min(min(u8)))*(u8-min(min(u8)))),250,250),"pic8.jpg");
-imwrite(reshape(uint8(255/(max(max(u9))-min(min(u9)))*(u9-min(min(u9)))),250,250),"pic9.jpg");
-imwrite(reshape(uint8(255/(max(max(u10))-min(min(u10)))*(u10-min(min(u10)))),250,250),"pic10.jpg");
 save facespace.txt facespace;
